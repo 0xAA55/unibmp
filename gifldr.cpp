0ff4659289984a9b8317381119176d2b8c5bfd60
0xAA55 <weixiaobao213@qq.com>
2024-06-14 09:24:40 +0800

    继续组织代码，把所有的 GIF 数据块的结构都封装出来。

--- gifldr.cpp
+++ gifldr.cpp
@@ -1,5 +1,6 @@
 #include "gifldr.hpp"
 
+#include <type_traits>
 #include <unordered_map>
 namespace CPPGIF
 {
@@ -20,15 +21,96 @@ namespace CPPGIF
 		{ 256, 7 },
 	};
 
-	GIFLoader::GIFLoader(const std::string& LoadFrom)
+	template<typename T>
+	size_t Read(std::istream& is, T& out)
 	{
-
+		is.read(reinterpret_cast<char*>(&out), sizeof(out));
+		return sizeof(out);
 	}
 
+	template<typename T>
+	size_t Read(std::istream& is, T* out_ptr, size_t count)
+	{
+		auto BytesRead = sizeof(T) * count;
+		is.read(reinterpret_cast<char*>(out_ptr), BytesRead);
+		return BytesRead;
+	}
+
+	template<typename T>
+	size_t ReadVector(std::istream& is, std::vector<T>& out)
+	{
+		auto BytesRead = sizeof(T) * out.size();
+		is.read(reinterpret_cast<char*>(&out[0]), BytesRead);
+		return BytesRead;
+	}
+
+	template<typename T>
+	size_t ReadVector(std::istream& is, std::vector<T>& out, size_t count)
+	{
+		out.resize(count);
+		auto BytesRead = sizeof(T) * count;
+		is.read(reinterpret_cast<char*>(&out[0]), BytesRead);
+		return BytesRead;
+	}
+
+	GIFLoader::GIFLoader(const std::string& LoadFrom)
+	{
+		std::ifstream ifs;
+		ifs.exceptions(std::ios::failbit | std::ios::badbit);
+		ifs.open(LoadFrom, std::ios::binary);
+		LoadGIF(ifs);
+	}
+
+	GIFLoader::GIFLoader(std::istream& LoadFrom)
+	{
+		LoadGIF(LoadFrom);
+	}
 
 	const std::string& GIFLoader::GetVersion() const
 	{
 		return Version;
+	}
+
+	const uint16_t GIFLoader::GetWidth() const
+	{
+		return LogicalScreenDescriptor.GetLogicalScreenWidth();
+	}
+
+	const uint16_t GIFLoader::GetHeight() const
+	{
+		return LogicalScreenDescriptor.GetLogicalScreenHeight();
+	}
+
+	const ColorTableArray& GIFLoader::GetGlobalColorTable(size_t& numColorsOut) const
+	{
+		numColorsOut = LogicalScreenDescriptor.SizeOfGlobalColorTable();
+		return LogicalScreenDescriptor.GetGlobalColorTable();
+	}
+
+	DataSubBlock::DataSubBlock(const uint8_t* src, uint8_t count)
+	{
+		if (!src) throw std::invalid_argument("GIF DataSubBlock::DataSubBlock(): Null pointer given.");
+		
+		BlockSize = count;
+		if (count) memcpy(&front(), src, count);
+	}
+
+	uint8_t DataSubBlock::GetBlockSize() const
+	{
+		return BlockSize;
+	}
+
+	DataSubBlock::DataSubBlock(std::istream& is)
+	{
+		Read(is, BlockSize);
+		Read(is, &front(), BlockSize);
+
+		if (BlockSize)
+		{ // 如果这个块有数据，它后面会有个 '\0'
+			auto Terminator = uint8_t();
+			Read(is, Terminator);
+			if (Terminator) throw UnexpectedData(std::string("DataSubBlock::DataSubBlock(): When reading: data terminator expected, got `") + std::to_string(Terminator) + "`");
+		}
 	}
 
 	const LogicalScreenDescriptorType& GIFLoader::GetLogicalScreenDescriptor() const
@@ -43,6 +125,19 @@ namespace CPPGIF
 		BackgroundColorIndex(BackgroundColorIndex),
 		GlobalColorTable(GlobalColorTable)
 	{
+	}
+
+	LogicalScreenDescriptorType::LogicalScreenDescriptorType(std::istream& LoadFrom)
+	{
+		Read(LoadFrom, LogicalScreenWidth);
+		Read(LoadFrom, LogicalScreenHeight);
+		Read(LoadFrom, Bitfields);
+		Read(LoadFrom, BackgroundColorIndex);
+		if (HasGCT())
+		{
+			GlobalColorTable = std::make_shared<ColorTableArray>();
+			Read(LoadFrom, &GlobalColorTable.get()[0], SizeOfGlobalColorTable());
+		}
 	}
 
 	uint8_t LogicalScreenDescriptorType::MakeBitfields(bool HasGCT, uint8_t ColorResolution, bool ColorIsSorted, size_t SizeOfGlobalColorTable)
@@ -86,22 +181,12 @@ namespace CPPGIF
 		return 1 << (Bitfields & 0x07);
 	}
 
-	void LogicalScreenDescriptorType::BreakBitfields(bool& HasGCT, uint8_t& ColorResolution, bool& ColorIsSorted, size_t& SizeOfGlobalColorTable)
+	void LogicalScreenDescriptorType::BreakBitfields(bool& HasGCT, uint8_t& ColorResolution, bool& ColorIsSorted, size_t& SizeOfGlobalColorTable) const
 	{
 		HasGCT = this->HasGCT();
 		ColorResolution = this->ColorResolution();
 		ColorIsSorted = this->ColorIsSorted();
 		SizeOfGlobalColorTable = this->SizeOfGlobalColorTable();
-	}
-
-	const uint16_t GIFLoader::GetWidth() const
-	{
-		return LogicalScreenDescriptor.GetLogicalScreenWidth();
-	}
-
-	const uint16_t GIFLoader::GetHeight() const
-	{
-		return LogicalScreenDescriptor.GetLogicalScreenHeight();
 	}
 
 	uint16_t LogicalScreenDescriptorType::GetLogicalScreenWidth() const
@@ -119,9 +204,55 @@ namespace CPPGIF
 		return *GlobalColorTable;
 	}
 
-	const ColorTableArray& GIFLoader::GetGlobalColorTable(uint32_t& numColorsOut) const
+	GraphicControlExtensionType::GraphicControlExtensionType(uint8_t BlockSize, uint8_t Bitfields, uint16_t DelayTime, uint8_t TransparentColorIndex, DataSubBlock* SubBlock_Optional) :
+		BlockSize(BlockSize), Bitfields(Bitfields), DelayTime(DelayTime), TransparentColorIndex(TransparentColorIndex)
 	{
-		return LogicalScreenDescriptor.GetGlobalColorTable();
+		if (SubBlock_Optional) SubBlock = *SubBlock_Optional;
 	}
+
+	GraphicControlExtensionType::GraphicControlExtensionType(std::istream& is)
+	{
+		Read(is, BlockSize);
+		Read(is, Bitfields);
+		Read(is, DelayTime);
+		Read(is, TransparentColorIndex);
+		SubBlock = DataSubBlock(is);
+	}
+
+	uint8_t GraphicControlExtensionType::MakeBitfields(DisposalMethodEnum DisposalMethod, bool ReactToUserInput, bool HasTransparency)
+	{
+		if (DisposalMethod < 0 || DisposalMethod >= 4)
+		{
+			throw std::invalid_argument(std::string("GraphicControlExtensionType::MakeBitfields(): Unknown frame disposal method `") + std::to_string(DisposalMethod) + "`.");
+		}
+		auto ret = uint8_t(0);
+		ret |= (uint8_t(DisposalMethod) << 2);
+		if (ReactToUserInput) ret |= 2;
+		if (HasTransparency) ret |= 1;
+		return ret;
+	}
+
+	void GraphicControlExtensionType::BreakBitfields(DisposalMethodEnum& DisposalMethod, bool& ReactToUserInput, bool& HasTransparency) const
+	{
+		DisposalMethod = GetDisposalMethod();
+		ReactToUserInput = ShouldReactToUserInput();
+		HasTransparency = this->HasTransparency();
+	}
+
+	GraphicControlExtensionType::DisposalMethodEnum GraphicControlExtensionType::GetDisposalMethod() const
+	{
+		return DisposalMethodEnum((Bitfields >> 2) & 7);
+	}
+
+	bool GraphicControlExtensionType::ShouldReactToUserInput() const
+	{
+		return (Bitfields & 2) == 2;
+	}
+
+	bool GraphicControlExtensionType::HasTransparency() const
+	{
+		return (Bitfields & 1) == 1;
+	}
+
 
 }
--- gifldr.hpp
+++ gifldr.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <memory>
 #include <stdexcept>
+#include <iostream>
 
 #include "unibmp.hpp"
 
@@ -28,11 +29,20 @@ namespace CPPGIF
 	constexpr size_t MaxColorTableItems = 256;
 	using ColorTableArray = std::array<ColorTableItem, MaxColorTableItems>;
 
-	struct AppExtension
+	struct DataSubBlock : public std::array<uint8_t, 255>
 	{
-		char Identifier[8];
-		char AuthenticationCode[3];
-		std::vector<uint8_t> Data;
+	protected:
+		uint8_t BlockSize = 0;
+
+	public:
+		DataSubBlock() = default;
+		DataSubBlock(const DataSubBlock& c) = default;
+		DataSubBlock(const uint8_t* src, uint8_t count);
+
+		uint8_t GetBlockSize() const;
+
+	public:
+		DataSubBlock(std::istream& is);
 	};
 
 	struct LogicalScreenDescriptorType
@@ -59,33 +69,80 @@ namespace CPPGIF
 		uint16_t GetLogicalScreenWidth() const;
 		uint16_t GetLogicalScreenHeight() const;
 		const ColorTableArray& GetGlobalColorTable() const;
+
+	public:
+		LogicalScreenDescriptorType(std::istream& LoadFrom);
+	};
+
+	struct GraphicControlExtensionType
+	{
+	protected:
+		uint8_t BlockSize = 0;
+		uint8_t Bitfields = 0;
+		uint16_t DelayTime = 0;
+		uint8_t TransparentColorIndex = 0;
+		DataSubBlock SubBlock;
+
+	public:
+		GraphicControlExtensionType() = default;
+		GraphicControlExtensionType(uint8_t BlockSize, uint8_t Bitfields, uint16_t DelayTime, uint8_t TransparentColorIndex, DataSubBlock* SubBlock_Optional);
+
+		enum DisposalMethodEnum
+		{
+			NoDisposalSpec = 0,
+			DoNotDispose = 1,
+			RestoreToBackgroundColor = 2,
+			RestoreToPrevious = 3,
+		};
+
+		static uint8_t MakeBitfields(DisposalMethodEnum DisposalMethod, bool ReactToUserInput, bool HasTransparency);
+		void BreakBitfields(DisposalMethodEnum DisposalMethod, bool ReactToUserInput, bool HasTransparency);
+
+		DisposalMethodEnum GetDisposalMethod() const;
+		bool ShouldReactToUserInput() const;
+		bool HasTransparency() const;
+
+	public:
+		GraphicControlExtensionType(std::istream& is);
+	};
+
+	struct ApplicationExtensionType
+	{
+		char Identifier[8];
+		char AuthenticationCode[3];
+		DataSubBlock ApplicationData;
 	};
 
 	class GIFLoader
 	{
 	public:
 
+
 	protected:
 		std::string Version; // gif87a / gif89a
 
 		// 逻辑屏幕描述符
 		LogicalScreenDescriptorType LogicalScreenDescriptor;
-		std::shared_ptr<AppExtension> ApplicationExtension;
 
-		
-		
+
+
+		std::shared_ptr<GraphicControlExtensionType> GraphicControlExtension;
+		std::shared_ptr<ApplicationExtensionType> ApplicationExtension;
 
 
 	public:
 		GIFLoader(const std::string& LoadFrom);
+		GIFLoader(std::istream& LoadFrom);
 
 		const std::string& GetVersion() const;
 		const uint16_t GetWidth() const;
 		const uint16_t GetHeight() const;
-		const ColorTableArray& GetGlobalColorTable(uint32_t& numColorsOut) const;
+		const ColorTableArray& GetGlobalColorTable(size_t& numColorsOut) const;
 
 		const LogicalScreenDescriptorType& GetLogicalScreenDescriptor() const;
 		
+	protected:
+		void LoadGIF(std::istream& LoadFrom);
 	};
 }
 
