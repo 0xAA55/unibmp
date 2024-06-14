03925bae5ec7b48a4e9991884ec27831a943ea1b
0xAA55 <weixiaobao213@qq.com>
2024-06-14 17:21:32 +0800

    实现根据给定的图片生成最适应该图片的调色板。

---
+++ PaletteGen.cpp
@@ -0,0 +1,151 @@
+#include "PaletteGen.hpp"
+
+#include <assert.h>
+
+namespace PaletteGeneratorLib
+{
+	static size_t GetNumColors(ColorNode& Node)
+	{
+		if (Node.IsLeaf) return 1;
+		
+		size_t ret = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			if (Node.SubNodes[i] == nullptr) continue;
+			ret += GetNumColors(*Node.SubNodes[i]);
+		}
+		return ret;
+	}
+
+	bool PaletteGenerator::ReduceNode(ColorNode& Node)
+	{
+		if (Node.IsLeaf)
+		{
+			return false;
+		}
+		else
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (!Node.SubNodes[j]) continue;
+				auto& Sub = *Node.SubNodes[j];
+				if (!Sub.IsLeaf) ReduceNode(Sub);
+				Node.RSum += Sub.RSum;
+				Node.GSum += Sub.GSum;
+				Node.BSum += Sub.BSum;
+				Node.NumPixels += Sub.NumPixels;
+				Node.SubNodes[j].reset();
+			}
+			Node.Sibling = nullptr;
+			Node.IsLeaf = true;
+			NumColors = GetNumColors(RootNode);
+			return true;
+		}
+	}
+
+	void GetDeepestNonLeafNode(ColorNode& Node, int CurLevel, int& MaxLevel, ColorNode*& MaxLevelNode)
+	{
+		if (CurLevel > MaxLevel)
+		{
+			MaxLevel = CurLevel;
+			MaxLevelNode = &Node;
+		}
+		for (int i = 0; i < 8; i++)
+		{
+			if (!Node.SubNodes[i]) continue;
+			if (Node.SubNodes[i]->IsLeaf) continue;
+			GetDeepestNonLeafNode(*Node.SubNodes[i], CurLevel + 1, MaxLevel, MaxLevelNode);
+		}
+	}
+
+	bool PaletteGenerator::ReduceTree()
+	{
+		int MaxLevel = 0;
+		ColorNode* NodePtr = nullptr;
+		GetDeepestNonLeafNode(RootNode, 0, MaxLevel, NodePtr);
+		if (NodePtr)
+		{
+			return ReduceNode(*NodePtr);
+		}
+		return false;
+	}
+
+	PaletteGenerator::PaletteGenerator(size_t MaxColors) :
+		MaxColors(MaxColors)
+	{
+	}
+
+	void PaletteGenerator::AddPixel(uint8_t R, uint8_t G, uint8_t B)
+	{
+		ColorNode* NodePtr = &RootNode;
+		int i;
+		for (i = 7; i >= 0; i--)
+		{
+			int index =
+				(((R >> i) & 1) << 0) |
+				(((G >> i) & 1) << 1) |
+				(((B >> i) & 1) << 2);
+
+			if (!NodePtr->IsLeaf)
+			{
+				if (!NodePtr->SubNodes[index])
+				{
+					NodePtr->SubNodes[index] = std::make_shared<ColorNode>();
+				}
+				NodePtr = NodePtr->SubNodes[index].get();
+				if (i == 0)
+				{
+					NodePtr->IsLeaf = true;
+					NumColors++;
+					break;
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+		assert(i >= 0);
+		auto& Node = *NodePtr;
+		Node.RSum += R;
+		Node.GSum += G;
+		Node.BSum += B;
+		Node.NumPixels++;
+		NumPixels++;
+		while (NumColors > MaxColors)
+		{
+			ReduceTree();
+		}
+	}
+
+	void PaletteGenerator::GetColors(const ColorNode& Node, std::vector<PaletteItem>& Palette)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			if (Node.SubNodes[i])
+			{
+				auto& SubNode = *Node.SubNodes[i];
+				if (SubNode.IsLeaf)
+				{
+					Palette.push_back(PaletteItem
+					{
+						uint8_t(SubNode.RSum / SubNode.NumPixels),
+						uint8_t(SubNode.GSum / SubNode.NumPixels),
+						uint8_t(SubNode.BSum / SubNode.NumPixels)
+					});
+				}
+				else
+				{
+					GetColors(SubNode, Palette);
+				}
+			}
+		}
+	}
+
+	std::vector<PaletteItem> PaletteGenerator::GetColors()
+	{
+		auto Palette = std::vector<PaletteItem>();
+		GetColors(RootNode, Palette);
+		return Palette;
+	}
+};
---
+++ PaletteGen.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "unibmp.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <array>
+
+namespace PaletteGeneratorLib
+{
+	struct ColorNode
+	{
+		uint64_t RSum = 0;
+		uint64_t GSum = 0;
+		uint64_t BSum = 0;
+		uint64_t NumPixels = 0;
+
+		bool IsLeaf = false;
+
+		std::array<std::shared_ptr<ColorNode>, 8> SubNodes;
+		ColorNode* Sibling = nullptr;
+
+		ColorNode() = default;
+	};
+
+	struct PaletteItem
+	{
+		uint8_t R;
+		uint8_t G;
+		uint8_t B;
+	};
+
+	class PaletteGenerator
+	{
+	protected:
+		ColorNode RootNode;
+		uint64_t NumPixels = 0;
+		size_t NumColors = 0;
+		size_t MaxColors = 256;
+
+		bool ReduceNode(ColorNode& Node);
+		bool ReduceTree();
+		static void GetColors(const ColorNode& Node, std::vector<PaletteItem>& Palette);
+
+	public:
+		PaletteGenerator() = default;
+		PaletteGenerator(size_t MaxColors);
+
+		void AddPixel(uint8_t R, uint8_t G, uint8_t B);
+
+		std::vector<PaletteItem> GetColors();
+	};
+};
+
