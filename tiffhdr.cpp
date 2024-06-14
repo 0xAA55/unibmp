848fd13564838d463311936d914813c49aa12ba6
0xAA55 <weixiaobao213@qq.com>
2024-06-14 09:24:30 +0800

    设计基本数据结构用于存储解析了的 TIFF header

--- tiffhdr.cpp
+++ tiffhdr.cpp
@@ -2,5 +2,362 @@
 
 namespace UniformBitmap
 {
+	const std::unordered_map<uint16_t, std::string> IFDTagToStr =
+	{
+		{0x0001, "InteroperabilityIndex"},
+		{0x0002, "InteroperabilityVersion"},
+		{0x00fe, "NewSubfileType"},
+		{0x00ff, "SubfileType"},
+		{0x0100, "ImageWidth"},
+		{0x0101, "ImageLength"},
+		{0x0102, "BitsPerSample"},
+		{0x0103, "Compression"},
+		{0x0106, "PhotometricInterpretation"},
+		{0x010e, "ImageDescription"},
+		{0x010f, "Make"},
+		{0x0110, "Model"},
+		{0x0111, "StripOffsets"},
+		{0x0112, "Orientation"},
+		{0x0115, "SamplesPerPixel"},
+		{0x0116, "RowsPerStrip"},
+		{0x0117, "StripByteConunts"},
+		{0x011a, "XResolution"},
+		{0x011b, "YResolution"},
+		{0x011c, "PlanarConfiguration"},
+		{0x0128, "ResolutionUnit"},
+		{0x012d, "TransferFunction"},
+		{0x0131, "Software"},
+		{0x0132, "DateTime"},
+		{0x013b, "Artist"},
+		{0x013d, "Predictor"},
+		{0x013e, "WhitePoint"},
+		{0x013f, "PrimaryChromaticities"},
+		{0x0142, "TileWidth"},
+		{0x0143, "TileLength"},
+		{0x0144, "TileOffsets"},
+		{0x0145, "TileByteCounts"},
+		{0x014a, "SubIFDs"},
+		{0x015b, "JPEGTables"},
+		{0x0201, "JpegIFOffset"},
+		{0x0202, "JpegIFByteCount"},
+		{0x0211, "YCbCrCoefficients"},
+		{0x0212, "YCbCrSubSampling"},
+		{0x0213, "YCbCrPositioning"},
+		{0x0214, "ReferenceBlackWhite"},
+		{0x1000, "RelatedImageFileFormat"},
+		{0x1001, "RelatedImageWidth"},
+		{0x1002, "RelatedImageLength"},
+		{0x828d, "CFARepeatPatternDim"},
+		{0x828e, "CFAPattern"},
+		{0x828f, "BatteryLevel"},
+		{0x8298, "Copyright"},
+		{0x829a, "ExposureTime"},
+		{0x829d, "FNumberunsigned"},
+		{0x83bb, "IPTC/NAA"},
+		{0x8769, "ExifOffset"},
+		{0x8773, "InterColorProfile"},
+		{0x8822, "ExposureProgram"},
+		{0x8824, "SpectralSensitivity"},
+		{0x8825, "GPSInfo"},
+		{0x8827, "ISOSpeedRatings"},
+		{0x8828, "OECF"},
+		{0x8829, "Interlace"},
+		{0x882a, "TimeZoneOffset"},
+		{0x882b, "SelfTimerMode"},
+		{0x9000, "ExifVersion"},
+		{0x9003, "DateTimeOriginal"},
+		{0x9004, "DateTimeDigitized"},
+		{0x9101, "ComponentsConfiguration"},
+		{0x9102, "CompressedBitsPerPixel"},
+		{0x9201, "ShutterSpeedValue"},
+		{0x9202, "ApertureValue"},
+		{0x9203, "BrightnessValue"},
+		{0x9204, "ExposureBiasValue"},
+		{0x9205, "MaxApertureValue"},
+		{0x9206, "SubjectDistance"},
+		{0x9207, "MeteringMode"},
+		{0x9208, "LightSource"},
+		{0x9209, "Flash"},
+		{0x920a, "FocalLength"},
+		{0x920b, "FlashEnergy"},
+		{0x920c, "SpatialFrequencyResponse"},
+		{0x920d, "Noise"},
+		{0x9211, "ImageNumber"},
+		{0x9212, "SecurityClassification"},
+		{0x9213, "ImageHistory"},
+		{0x9214, "SubjectLocation"},
+		{0x9215, "ExposureIndex"},
+		{0x9216, "TIFF/EPStandardID"},
+		{0x927c, "MakerNote"},
+		{0x9286, "UserComment"},
+		{0x9290, "SubsecTime"},
+		{0x9291, "SubsecTimeOriginal"},
+		{0x9292, "SubsecTimeDigitized"},
+		{0xa000, "FlashPixVersion"},
+		{0xa001, "ColorSpace"},
+		{0xa002, "ExifImageWidth"},
+		{0xa003, "ExifImageHeight"},
+		{0xa004, "RelatedSoundFile"},
+		{0xa005, "ExifInteroperabilityOffset"},
+		{0xa20b, "FlashEnergy"},
+		{0xa20c, "SpatialFrequencyResponse"},
+		{0xa20e, "FocalPlaneXResolution"},
+		{0xa20f, "FocalPlaneYResolution"},
+		{0xa210, "FocalPlaneResolutionUnit"},
+		{0xa214, "SubjectLocation"},
+		{0xa215, "ExposureIndex"},
+		{0xa217, "SensingMethod"},
+		{0xa300, "FileSource"},
+		{0xa301, "SceneType"},
+		{0xa302, "CFAPattern"},
+	};
 
+	const std::unordered_map<std::string, uint16_t> IFDTagFromStr =
+	{
+		{"InteroperabilityIndex", 0x0001},
+		{"InteroperabilityVersion", 0x0002},
+		{"NewSubfileType", 0x00fe},
+		{"SubfileType", 0x00ff},
+		{"ImageWidth", 0x0100},
+		{"ImageLength", 0x0101},
+		{"BitsPerSample", 0x0102},
+		{"Compression", 0x0103},
+		{"PhotometricInterpretation", 0x0106},
+		{"ImageDescription", 0x010e},
+		{"Make", 0x010f},
+		{"Model", 0x0110},
+		{"StripOffsets", 0x0111},
+		{"Orientation", 0x0112},
+		{"SamplesPerPixel", 0x0115},
+		{"RowsPerStrip", 0x0116},
+		{"StripByteConunts", 0x0117},
+		{"XResolution", 0x011a},
+		{"YResolution", 0x011b},
+		{"PlanarConfiguration", 0x011c},
+		{"ResolutionUnit", 0x0128},
+		{"TransferFunction", 0x012d},
+		{"Software", 0x0131},
+		{"DateTime", 0x0132},
+		{"Artist", 0x013b},
+		{"Predictor", 0x013d},
+		{"WhitePoint", 0x013e},
+		{"PrimaryChromaticities", 0x013f},
+		{"TileWidth", 0x0142},
+		{"TileLength", 0x0143},
+		{"TileOffsets", 0x0144},
+		{"TileByteCounts", 0x0145},
+		{"SubIFDs", 0x014a},
+		{"JPEGTables", 0x015b},
+		{"JpegIFOffset", 0x0201},
+		{"JpegIFByteCount", 0x0202},
+		{"YCbCrCoefficients", 0x0211},
+		{"YCbCrSubSampling", 0x0212},
+		{"YCbCrPositioning", 0x0213},
+		{"ReferenceBlackWhite", 0x0214},
+		{"RelatedImageFileFormat", 0x1000},
+		{"RelatedImageWidth", 0x1001},
+		{"RelatedImageLength", 0x1002},
+		{"CFARepeatPatternDim", 0x828d},
+		{"CFAPattern", 0x828e},
+		{"BatteryLevel", 0x828f},
+		{"Copyright", 0x8298},
+		{"ExposureTime", 0x829a},
+		{"FNumberunsigned", 0x829d},
+		{"IPTC/NAA", 0x83bb},
+		{"ExifOffset", 0x8769},
+		{"InterColorProfile", 0x8773},
+		{"ExposureProgram", 0x8822},
+		{"SpectralSensitivity", 0x8824},
+		{"GPSInfo", 0x8825},
+		{"ISOSpeedRatings", 0x8827},
+		{"OECF", 0x8828},
+		{"Interlace", 0x8829},
+		{"TimeZoneOffset", 0x882a},
+		{"SelfTimerMode", 0x882b},
+		{"ExifVersion", 0x9000},
+		{"DateTimeOriginal", 0x9003},
+		{"DateTimeDigitized", 0x9004},
+		{"ComponentsConfiguration", 0x9101},
+		{"CompressedBitsPerPixel", 0x9102},
+		{"ShutterSpeedValue", 0x9201},
+		{"ApertureValue", 0x9202},
+		{"BrightnessValue", 0x9203},
+		{"ExposureBiasValue", 0x9204},
+		{"MaxApertureValue", 0x9205},
+		{"SubjectDistance", 0x9206},
+		{"MeteringMode", 0x9207},
+		{"LightSource", 0x9208},
+		{"Flash", 0x9209},
+		{"FocalLength", 0x920a},
+		{"FlashEnergy", 0x920b},
+		{"SpatialFrequencyResponse", 0x920c},
+		{"Noise", 0x920d},
+		{"ImageNumber", 0x9211},
+		{"SecurityClassification", 0x9212},
+		{"ImageHistory", 0x9213},
+		{"SubjectLocation", 0x9214},
+		{"ExposureIndex", 0x9215},
+		{"TIFF/EPStandardID", 0x9216},
+		{"MakerNote", 0x927c},
+		{"UserComment", 0x9286},
+		{"SubsecTime", 0x9290},
+		{"SubsecTimeOriginal", 0x9291},
+		{"SubsecTimeDigitized", 0x9292},
+		{"FlashPixVersion", 0xa000},
+		{"ColorSpace", 0xa001},
+		{"ExifImageWidth", 0xa002},
+		{"ExifImageHeight", 0xa003},
+		{"RelatedSoundFile", 0xa004},
+		{"ExifInteroperabilityOffset", 0xa005},
+		{"FlashEnergy", 0xa20b},
+		{"SpatialFrequencyResponse", 0xa20c},
+		{"FocalPlaneXResolution", 0xa20e},
+		{"FocalPlaneYResolution", 0xa20f},
+		{"FocalPlaneResolutionUnit", 0xa210},
+		{"SubjectLocation", 0xa214},
+		{"ExposureIndex", 0xa215},
+		{"SensingMethod", 0xa217},
+		{"FileSource", 0xa300},
+		{"SceneType", 0xa301},
+		{"CFAPattern", 0xa302},
+	};
+
+	bool IFDField::operator==(const IFDField& other) const
+	{
+		if (Type != other.Type) return false;
+		switch (Type)
+		{
+		case IFDFieldFormat::Undefined:
+		case IFDFieldFormat::Unknown: return UnknownData == other.UnknownData;
+		case IFDFieldFormat::AsciiString: return String == other.String;
+		case IFDFieldFormat::UByte: return Literal.UByte == other.Literal.UByte;
+		case IFDFieldFormat::UShort: return Literal.UShort == other.Literal.UShort;
+		case IFDFieldFormat::ULong: return Literal.ULong == other.Literal.ULong;
+		case IFDFieldFormat::URational: return Literal.URational == other.Literal.URational;
+		case IFDFieldFormat::SByte: return Literal.SByte == other.Literal.SByte;
+		case IFDFieldFormat::SShort: return Literal.SShort == other.Literal.SShort;
+		case IFDFieldFormat::SLong: return Literal.SLong == other.Literal.SLong;
+		case IFDFieldFormat::SRational: return Literal.SRational == other.Literal.SRational;
+		case IFDFieldFormat::Float: return Literal.Float == other.Literal.Float;
+		case IFDFieldFormat::Double: return Literal.Double == other.Literal.Double;
+		default: throw std::invalid_argument("Unknown IFD field type.");
+		}
+	}
+
+
+	IFDField::IFDField(IFDFieldFormat Type, int32_t Number) :
+		Type(Type)
+	{
+		switch (Type)
+		{
+		case IFDFieldFormat::SByte: Literal.SByte = int8_t(Number); break;
+		case IFDFieldFormat::SShort: Literal.SShort = int16_t(Number); break;
+		case IFDFieldFormat::SLong: Literal.SLong = int32_t(Number); break;
+		case IFDFieldFormat::UByte: Literal.UByte = uint8_t(Number); break;
+		case IFDFieldFormat::UShort: Literal.UShort = uint16_t(Number); break;
+		case IFDFieldFormat::ULong: Literal.ULong = uint32_t(Number); break;
+		default: throw std::invalid_argument("Unknown IFD field type.");
+		}
+	}
+	IFDField::IFDField(IFDFieldFormat Type, uint32_t Number) :
+		Type(Type)
+	{
+		switch (Type)
+		{
+		case IFDFieldFormat::SByte: Literal.SByte = int8_t(Number); break;
+		case IFDFieldFormat::SShort: Literal.SShort = int16_t(Number); break;
+		case IFDFieldFormat::SLong: Literal.SLong = int32_t(Number); break;
+		case IFDFieldFormat::UByte: Literal.UByte = uint8_t(Number); break;
+		case IFDFieldFormat::UShort: Literal.UShort = uint16_t(Number); break;
+		case IFDFieldFormat::ULong: Literal.ULong = uint32_t(Number); break;
+		default: throw std::invalid_argument("Unknown IFD field type.");
+		}
+	}
+	IFDField::IFDField(IFDFieldFormat Type, double Number) :
+		Type(Type)
+	{
+		switch (Type)
+		{
+		case IFDFieldFormat::Float: Literal.Float = float(Number); break;
+		case IFDFieldFormat::Double: Literal.Double = double(Number); break;
+		default: throw std::invalid_argument("Unknown IFD field type.");
+		}
+	}
+	IFDField::IFDField(const Rational& Rational) :
+		Type(IFDFieldFormat::SRational)
+	{
+		Literal.SRational = Rational;
+	}
+	IFDField::IFDField(const URational& URational) :
+		Type(IFDFieldFormat::URational)
+	{
+		Literal.URational = URational;
+	}
+	IFDField::IFDField(const TIFFDateTime& DateTime) :
+		IFDField(std::string(DateTime))
+	{
+	}
+	IFDField::IFDField(const std::string& String) :
+		Type(IFDFieldFormat::AsciiString),
+		String(String)
+	{
+	}
+
+	void IFD::WriteField(uint16_t Tag, const IFDField& field)
+	{
+		Fields[Tag] = field;
+	}
+
+	void IFD::WriteField(const std::string& TagString, const IFDField& field)
+	{
+		Fields[IFDTagFromStr.at(TagString)] = field;
+	}
+
+	TIFFDateTime::operator std::string() const
+	{
+		char buf[] = "YYYY:MM:DD HH:MM:SS";
+		size_t i = 0;
+#define TIFFDateTime_Write(member) do {memcpy(&buf[i], &member, sizeof member); i += sizeof member;} while (0)
+		TIFFDateTime_Write(YYYY); i++;
+		TIFFDateTime_Write(MM); i++;
+		TIFFDateTime_Write(DD); i++;
+		TIFFDateTime_Write(hh); i++;
+		TIFFDateTime_Write(mm); i++;
+		TIFFDateTime_Write(ss);
+#undef TIFFDateTime_Write
+		return buf;
+	}
+
+	TIFFHeader ConstuctTIFFHeader
+	(
+		const std::string& ImageDescription,
+		const std::string& Make,
+		const std::string& Model,
+		const URational* XResolution,
+		const URational* YResolution,
+		const std::string& Software,
+		const TIFFDateTime* DateTime,
+		const std::string& CopyRight,
+		std::shared_ptr<IFD> SubIFD
+	)
+	{
+		auto IFD0 = IFD();
+
+		if (ImageDescription.length()) IFD0.WriteField("ImageDescription", ImageDescription);
+		if (Make.length()) IFD0.WriteField("Make", Make);
+		if (Model.length()) IFD0.WriteField("Model", Model);
+		if (XResolution) IFD0.WriteField("XResolution", *XResolution);
+		if (YResolution) IFD0.WriteField("YResolution", *YResolution);
+		IFD0.WriteField("Software", Software);
+		if (DateTime) IFD0.WriteField("DateTime", *DateTime);
+		if (CopyRight.length()) IFD0.WriteField("CopyRight", CopyRight);
+		IFD0.SubIFD = SubIFD;
+
+		return { IFD0 };
+	}
+
+	TIFFHeader ParseTIFFHeader(const uint8_t* TIFFData, size_t& TIFFDataSize)
+	{
+
+	}
 }
--- tiffhdr.hpp
+++ tiffhdr.hpp
@@ -1,47 +1,141 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include <unordered_map>
 
 namespace UniformBitmap
 {
-	enum class IFD0_Orientation
+	// https://web.archive.org/web/20111025004429/http://park2.wakwak.com/~tsuruzoh/Computer/Digicams/exif-e.html
+
+	struct Rational
+	{
+		int32_t Numerator = 0;
+		int32_t Denominator = 1;
+
+		Rational() = default;
+		Rational(const Rational& c) = default;
+		bool operator==(const Rational& other) const = default;
+	};
+	struct URational
+	{
+		uint32_t Numerator = 0;
+		uint32_t Denominator = 1;
+
+		URational() = default;
+		URational(const URational& c) = default;
+		bool operator==(const URational& other) const = default;
+	};
+
+	enum class IFDFieldFormat
 	{
 		Unknown = 0,
-		TopLeft = 1,
-		TopRight = 2,
-		BottomRight = 3,
-		BottomLeft = 4,
-		LeftTop = 5,
-		RightTop = 6,
-		RightBottom = 7,
-		LeftBottom = 8
+		UByte = 1,
+		AsciiString = 2,
+		UShort = 3,
+		ULong = 4,
+		URational = 5,
+		SByte = 6,
+		Undefined = 7,
+		SShort = 8,
+		SLong = 9,
+		SRational = 10,
+		Float = 11,
+		Double = 12
 	};
 
-	struct IFD0
+	struct TIFFDateTime
 	{
-		struct Rational
-		{
-			int32_t Numerator;
-			int32_t Denominator;
-		};
-		struct URational
-		{
-			uint32_t Numerator;
-			uint32_t Denominator;
-		};
+		char YYYY[4] = {};
+		char MM[2] = {};
+		char DD[2] = {};
+		char hh[2] = {};
+		char mm[2] = {};
+		char ss[2] = {};
 
-		std::string ImageDescription;
-		std::string Make;
-		std::string Model;
-		IFD0_Orientation Orientation;
-		URational XResolution;
-		URational YResolution;
-		uint16_t ResolutionUnit;
+		operator std::string() const;
 	};
 
-	class TIFFHeader
+	class IFDField
 	{
 	public:
+		IFDFieldFormat Type = IFDFieldFormat::Unknown;
+		union
+		{
+			uint8_t UByte;
+			uint16_t UShort;
+			uint32_t ULong;
+			URational URational;
+			int8_t SByte;
+			int16_t SShort;
+			int32_t SLong;
+			Rational SRational;
+			float Float;
+			double Double;
+		} Literal = {};
+		std::string String;
+		std::vector<uint8_t> UnknownData;
 
+		IFDField() = default;
+		IFDField(const IFDField& c) = default;
+		bool operator==(const IFDField& other) const;
+
+		IFDField(IFDFieldFormat Type, int32_t Number);
+		IFDField(IFDFieldFormat Type, uint32_t Number);
+		IFDField(IFDFieldFormat Type, double Number);
+
+		IFDField(const Rational& Rational);
+		IFDField(const URational& URational);
+		IFDField(const TIFFDateTime& DateTime);
+		IFDField(const std::string& String);
 	};
+
+	using IFDData = std::unordered_map<uint16_t, IFDField>;
+
+	struct IFD
+	{
+		IFDData Fields;
+
+		std::shared_ptr<IFD> SubIFD;
+
+		IFD() = default;
+		bool operator==(const IFD& other) const = default;
+
+		void WriteField(uint16_t Tag, const IFDField& field);
+		void WriteField(const std::string& TagString, const IFDField& field);
+	};
+
+	using TIFFHeader = std::vector<IFD>;
+
+	extern const std::unordered_map<uint16_t, std::string> IFDTagToStr;
+	extern const std::unordered_map<std::string, uint16_t> IFDTagFromStr;
+
+	// 函数：建立默认的 TIFF 头
+	TIFFHeader ConstuctTIFFHeader
+	(
+		const std::string& ImageDescription = "",
+		const std::string& Make = "",
+		const std::string& Model = "",
+		const URational* XResolution = nullptr,
+		const URational* YResolution = nullptr,
+		const std::string& Software = "",
+		const TIFFDateTime* DateTime = nullptr,
+		const std::string& CopyRight = "",
+		std::shared_ptr<IFD> SubIFD = nullptr
+	);
+
+	// 函数：解析 TIFF 头
+	// 参数：
+	//   TIFFData：一个指针指向内存中的图片文件中的 TIFF 数据部分（即开头是 II 或者 MM 的数据）
+	//   TIFFDataSize：TIFF 的数据的大小，如果给 0 表示不知道大小。
+	//     这个参数同时用于接收实际读取的 TIFF 数据的大小
+	// 返回值：经过初步解析的结构化的 TIFF 数据
+	TIFFHeader ParseTIFFHeader(const uint8_t* TIFFData, size_t& TIFFDataSize);
+
+	// 函数：将解析好的 TIFF 头再做成字节数组
+	// 参数：
+	//   TIFFHdr：由 ParseTIFFHeader 返回的解析好的 TIFF 头。
+	std::vector<uint8_t> StoreTIFFHeader(const TIFFHeader& TIFFHdr);
 }
