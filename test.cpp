b83ca654f573f371930f3a4eac71d7b848bb0fd2
0xAA55 <weixiaobao213@qq.com>
2024-06-14 09:24:39 +0800

    添加一些测试代码来加载 gif 文件。

--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
 #include "unibmp.hpp"
 
-
+using namespace UniformBitmap;
 
 
 int main(int argc, char** argv)
 {
+	auto Img = Image_RGBA8("Rotating_earth_(large).gif");
+	Img.SaveToPNG("test.png");
+
 	return 0;
 }
 
